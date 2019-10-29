b060f1a031a15cd5fde0fa8853f4463aeda347c1
Esteban BLANC <esteban.blanc@epita.fr>
2019-10-29 18:52:42 +0100

    Add url cli arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opt
+{
+    #[structopt(name = "url", required = true)]
+    origin: String,
 }
+
+fn main() {
+    let opt = Opt::from_args();
+}
+
