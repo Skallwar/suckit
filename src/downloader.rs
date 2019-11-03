1060696bf0320f10eabf57e2e5fbbac16f19beb2
Skallwar <estblcsk@gmail.com>
2019-11-03 22:46:10 +0100

    Add tests to downloader.rs

--- src/downloader.rs
+++ src/downloader.rs
@@ -5,3 +5,17 @@ use reqwest::Url;
 pub fn download_url(url: Url) -> Result<String, reqwest::Error> {
     reqwest::get(url)?.text()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_download_url() {
+        let url: Url = Url::parse("https://lwn.net").unwrap();
+        match download_url(url) {
+            Err(e) => assert!(false, "Fail to download lwn.net: {:?}", e),
+            _ => {}
+        }
+    }
+}
