f5e978fc28f4eff1a87562818e39b0ef619cd4d0
Esteban Blanc <estblcsk@gmail.com>
2020-04-09 22:38:48 +0200

    parser: checkout to master

--- src/parser.rs
+++ src/parser.rs
@@ -1,24 +1,12 @@
-use kuchiki::traits::*;
-use kuchiki::NodeData::*;
 use regex::Regex;
 use reqwest::Url;
-use std::borrow::BorrowMut;
 
 pub fn find_urls(str: String) -> Vec<String> {
-    let html = kuchiki::parse_html().one(str);
-    let elm = html.select_first("[src]").unwrap();
-    let node = elm.as_node();
-    let elm = node.as_element().unwrap();
-    let mut attributes_map = elm.attributes.try_borrow_mut().unwrap();
-    let string = attributes_map.get_mut("src").unwrap();
-    string.clear();
-    string.push_str("lololo");
-
-    println!("{:?}", attributes_map.get_mut("src").unwrap());
-
-    // elm.lol();
-
-    vec![]
+    let regex = Regex::new(r#"(href|src) *= *"([^ "]*)""#).unwrap();
+    regex
+        .captures_iter(&str)
+        .map(|matched| String::from(&matched[2]))
+        .collect()
 }
 
 #[cfg(test)]
