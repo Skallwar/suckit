a31b957d5cf2ca75dc0125f6659f759c922c663a
cohenarthur <arthur.cohen@epita.fr>
2020-04-09 21:13:56 +0200

    scraper: Add tests

--- src/scraper.rs
+++ src/scraper.rs
@@ -1,5 +1,5 @@
-use std::collections::VecDeque;
 use reqwest::Url;
+use std::collections::VecDeque;
 
 static DEFAULT_CAPACITY: usize = 128;
 
@@ -25,12 +25,68 @@ impl Scraper {
     }
 
     /// Remove an element from the scraper's queue
-    pub fn pop(&mut self) -> Url {
-        let res = self.queue.pop_front().unwrap();
-
-        res
+    pub fn pop(&mut self) -> Option<Url> {
+        self.queue.pop_front()
     }
 
     /// Run through the queue and complete it
     pub fn run() {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn push_one() {
+        let mut s = Scraper::new();
+
+        s.push(Url::parse("https://example.com").unwrap());
+    }
+
+    #[test]
+    fn pop_one() {
+        let mut s = Scraper::new();
+
+        s.push(Url::parse("https://example.com").unwrap());
+
+        match s.pop() {
+            None => assert!(false),
+            Some(url) => assert_eq!(url.to_string(), "https://example.com"),
+        };
+    }
+
+    #[test]
+    fn pop_empty() {
+        let mut s = Scraper::new();
+
+        match s.pop() {
+            None => assert!(true),
+            Some(invalid) => assert!(false),
+        };
+    }
+
+    #[test]
+    fn order() {
+        let mut s = Scraper::new();
+
+        s.push(Url::parse("http://0.com").unwrap());
+        s.push(Url::parse("http://1.com").unwrap());
+        s.push(Url::parse("http://2.com").unwrap());
+
+        match s.pop() {
+            None => assert!(false),
+            Some(url) => assert_eq!(url.to_string(), "http://0.com"),
+        }
+
+        match s.pop() {
+            None => assert!(false),
+            Some(url) => assert_eq!(url.to_string(), "http://1.com"),
+        }
+
+        match s.pop() {
+            None => assert!(false),
+            Some(url) => assert_eq!(url.to_string(), "http://2.com"),
+        }
+    }
 }
