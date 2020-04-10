f7e520d2acc63d7068b91397455c29eb7e7b2482
cohenarthur <arthur.cohen@epita.fr>
2020-04-10 15:43:58 +0200

    downloader: Remove erroneous formating

--- src/downloader.rs
+++ src/downloader.rs
@@ -39,6 +39,7 @@ mod tests {
         margin: 0;
         padding: 0;
         font-family: -apple-system, system-ui, BlinkMacSystemFont, \"Segoe UI\", \"Open Sans\", \"Helvetica Neue\", Helvetica, Arial, sans-serif;
+        
     }
     div {
         width: 600px;
@@ -58,7 +59,7 @@ mod tests {
             width: auto;
         }
     }
-    </style>
+    </style>    
 </head>
 
 <body>
