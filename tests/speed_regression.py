c1495bd802b07c0f928fae75d361aef726d0a134
cohenarthur <arthur.cohen@epita.fr>
2020-04-16 16:11:29 +0200

    scraper: Need to add 10 tries and mean to calculate time

--- tests/speed_regression.py
+++ tests/speed_regression.py
@@ -6,20 +6,38 @@ TEST_RETRIES = 5
 # File to store the results
 FILENAME = "speed.csv"
 
+# Path to the suckit binary
+SUCKIT = "suckit"
+
+# URL to download
+URL = "https://books.toscrape.com"
+
+# Path to store the downloaded data
+PATH = "/tmp/suckit_speed"
+
 import argparse
 import csv
+import os
+import shutil
+import subprocess
+import time
 
 def parse_args():
     global FILENAME
+    global SUCKIT
 
     parser = argparse.ArgumentParser(description = "SuckIT benchmark")
 
     parser.add_argument("-f", "--filename", action = "store", type = str, help = f"result file (default_value = '{FILENAME}')")
+    parser.add_argument("-s", "--suckit", action = "store", type = str, help = f"path to the suckit binary (default_value = '{SUCKIT}')")
 
     args = parser.parse_args()
 
     if args.filename:
         FILENAME = args.filename
+
+    if args.suckit:
+        SUCKIT = args.suckit
 
 def load_prev_result(filename):
     try:
@@ -31,7 +49,7 @@ def load_prev_result(filename):
             reader = csv.reader([line], delimiter = ",")
             for row in reader:
                 for cell in row:
-                    res.append(int(cell))
+                    res.append(float(cell))
 
     except IOError:
         print(f"Could not read file {filename}")
@@ -42,16 +60,37 @@ def load_prev_result(filename):
 def write_new_result(filename, result):
     try:
         with open(filename, "a") as csv:
-            csv.write("{}, {}, {}".format(result[0], result[1], result[2]))
+            csv.write("{}, {}, {}\n".format(result[0], result[1], result[2]))
     except IOError:
         print(f"Could not write to file {filename}")
         exit(1)
 
 def compute_new_result():
-    return [12, 6, 3]
+    thread_counts = ["1", "2", "4"]
+    res = []
+
+    for count in thread_counts:
+        start_time = time.time()
+
+        print(f"-j {count}")
+        print(f"-o {PATH}")
+        subprocess.Popen([SUCKIT, "-j {}".format(count), "-o {}".format(PATH), URL],
+                stdout = open("/dev/null", "w"), shell = False).pid
+
+        end_time = time.time()
+
+        res.append(end_time - start_time)
+
+    return res
 
 def main():
     parse_args()
+
+    # If the directory already exists, just ignore it
+    try:
+        os.mkdir(PATH)
+    except OSError:
+        pass
 
     test_names = ["Single thread", "Two threads", "Four threads"]
     old_result = load_prev_result(FILENAME)
@@ -64,6 +103,8 @@ def main():
         print(f"{test_names[i]} was {speed_up} quicker")
 
     write_new_result(FILENAME, new_result)
+
+    shutil.rmtree(PATH)
 
 if __name__ == "__main__":
     main()
