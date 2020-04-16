d0890a920a0504f5a860a50b3697013afe9c3d8c
cohenarthur <arthur.cohen@epita.fr>
2020-04-16 13:25:52 +0200

    speed_regression: WIP: Add base for speed benchmark

---
+++ tests/speed_regression.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+# Number of tests for each bench-set
+TEST_RETRIES = 5
+
+# File to store the results
+FILENAME = "speed.csv"
+
+def parse_args():
+    global FILENAME
+
+    parser = argparse.ArgumentParser(description = "SuckIT benchmark")
+
+def main():
+    load_prev_result(filename)
+
+if __name__ == "__main_":
+    main()
