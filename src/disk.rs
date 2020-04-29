79c9a5d4bb3a597198e877771afdc44585af2b86
Esteban Blanc <estblcsk@gmail.com>
2020-04-29 22:24:26 +0200

    misc: Run clippy and rustfmt

--- src/disk.rs
+++ src/disk.rs
@@ -3,32 +3,41 @@ use std::io::Write;
 use std::path::PathBuf;
 
 //TODO: Recover insted of panic
-pub fn save_file(file_name: &String, content: &[u8], path: &Option<PathBuf>) {
+pub fn save_file(file_name: &str, content: &[u8], path: &Option<PathBuf>) {
     let path = match path {
         Some(path) => path.join(file_name),
         None => PathBuf::from(file_name),
     };
 
-    match path.parent() {
-        Some(parent) => match fs::create_dir_all(parent) {
+    if let Some(parent) = path.parent() {
+        match fs::create_dir_all(parent) {
             Err(err) => panic!("Couldn't create folder {}: {}", parent.display(), err),
             Ok(()) => (),
-        },
-        None => (),
+        }
     }
+    // match path.parent() {
+    //     Some(parent) => match fs::create_dir_all(parent) {
+    //         Err(err) => panic!("Couldn't create folder {}: {}", parent.display(), err),
+    //         Ok(()) => (),
+    //     },
+    //     None => (),
+    // }
 
     let mut file = match fs::File::create(&path) {
         Err(err) => panic!("Couldn't create {}: {}", path.display(), err),
         Ok(file) => file,
     };
 
-    match file.write_all(content) {
-        Err(err) => panic!("Couldn't write to {}: {}", path.display(), err),
-        Ok(_) => (),
-    };
+    if let Err(err) = file.write_all(content) {
+        panic!("Couldn't write to {}: {}", path.display(), err);
+    }
+    // match file.write_all(content) {
+    //     Err(err) => panic!("Couldn't write to {}: {}", path.display(), err),
+    //     Ok(_) => (),
+    // };
 }
 
-pub fn symlink(source: &String, destination: &String, path: &Option<PathBuf>) {
+pub fn symlink(source: &str, destination: &str, path: &Option<PathBuf>) {
     let destination = match path {
         Some(path) => path.join(destination),
         None => PathBuf::from(destination),
