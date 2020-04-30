3dfcbcd6a81e2e47fac3680cd778e7c40c0c15a1
cohenarthur <arthur.cohen@epita.fr>
2020-04-30 17:31:10 +0200

    args: Add 'help' section

--- src/args.rs
+++ src/args.rs
@@ -5,22 +5,22 @@ use url::Url;
 
 #[derive(Debug, StructOpt)]
 pub struct Args {
-    #[structopt(name = "url", parse(try_from_str))]
+    #[structopt(name = "url", parse(try_from_str), help = "URL to start scraping from")]
     pub origin: Url,
 
-    #[structopt(short, long, parse(from_os_str))]
+    #[structopt(short, long, parse(from_os_str), help = "Output directory")]
     pub output: Option<PathBuf>,
 
-    #[structopt(short, long, default_value = "1")]
+    #[structopt(short, long, default_value = "1", help = "Maximum number of threads to use concurrently")]
     pub jobs: usize,
 
-    #[structopt(short, long, default_value = "5")]
+    #[structopt(short, long, default_value = "5", help = "Maximum recursion depth to reach when visiting")]
     pub depth: usize,
 
-    #[structopt(short, long, default_value = "20")]
+    #[structopt(short, long, default_value = "20", help = "Maximum amount of retries on download failure")]
     pub tries: usize,
 
-    #[structopt(short, long)]
+    #[structopt(short, long, help = "Enable more information regarding the download")]
     pub verbose: bool,
 }
 
