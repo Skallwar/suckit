6e93171185240096133af25bf05bcbb1969beaec
CohenArthur <arthur.cohen@epita.fr>
2020-05-01 13:48:39 +0200

    Better help for CLI arguments (#56)
    * args: Add 'help' section
    
    * benchmark: Add more data
    
    * Update src/args.rs
    
    Co-authored-by: Esteban Blanc <estblcsk@gmail.com>
    
    * Update src/args.rs
    
    Co-authored-by: Esteban Blanc <estblcsk@gmail.com>
    
    Co-authored-by: Esteban Blanc <estblcsk@gmail.com>

--- src/args.rs
+++ src/args.rs
@@ -7,27 +7,27 @@ use url::Url;
 #[derive(Debug, StructOpt)]
 pub struct Args {
     ///Entry point of scraping
-    #[structopt(name = "url", parse(try_from_str))]
+    #[structopt(name = "url", parse(try_from_str), help = "Entry point of the scraping")]
     pub origin: Url,
 
     ///Output directory
-    #[structopt(short, long, parse(from_os_str))]
+    #[structopt(short, long, parse(from_os_str), help = "Output directory")]
     pub output: Option<PathBuf>,
 
     ///Number of threads/workers
-    #[structopt(short, long, default_value = "1")]
+    #[structopt(short, long, default_value = "1", help = "Maximum number of threads to use concurrently")]
     pub jobs: usize,
 
     ///Max depth of scraping recursion
-    #[structopt(short, long, default_value = "5")]
+    #[structopt(short, long, default_value = "5", help = "Maximum recursion depth to reach when visiting")]
     pub depth: usize,
 
     ///Number of retries when downloading a page fails
-    #[structopt(short, long, default_value = "20")]
+    #[structopt(short, long, default_value = "20", help = "Maximum amount of retries on download failure")]
     pub tries: usize,
 
-    ///Show logs
-    #[structopt(short, long)]
+    ///Show all logs
+    #[structopt(short, long, help = "Enable more information regarding the scraping process")]
     pub verbose: bool,
 }
 
