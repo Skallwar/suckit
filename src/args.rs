03000cc4c131ca420ea4e315626590e1dea4160a
Atul Bhosale <atul1bhosale@gmail.com>
2020-05-13 23:32:53 +0530

    Format code using 'cargo fmt'

--- src/args.rs
+++ src/args.rs
@@ -7,7 +7,11 @@ use url::Url;
 #[derive(Debug, StructOpt)]
 pub struct Args {
     ///Entry point of scraping
-    #[structopt(name = "url", parse(try_from_str), help = "Entry point of the scraping")]
+    #[structopt(
+        name = "url",
+        parse(try_from_str),
+        help = "Entry point of the scraping"
+    )]
     pub origin: Url,
 
     ///Output directory
@@ -15,31 +19,63 @@ pub struct Args {
     pub output: Option<PathBuf>,
 
     ///Number of threads/workers
-    #[structopt(short, long, default_value = "1", help = "Maximum number of threads to use concurrently")]
+    #[structopt(
+        short,
+        long,
+        default_value = "1",
+        help = "Maximum number of threads to use concurrently"
+    )]
     pub jobs: usize,
 
     ///Max depth of scraping recursion
-    #[structopt(short, long, default_value = "-1", help = "Maximum recursion depth to reach when visiting. -1 is the default and will go as far as it can")]
+    #[structopt(
+        short,
+        long,
+        default_value = "-1",
+        help = "Maximum recursion depth to reach when visiting. -1 is the default and will go as far as it can"
+    )]
     pub depth: i32,
 
     ///Number of retries when downloading a page fails
-    #[structopt(short, long, default_value = "20", help = "Maximum amount of retries on download failure")]
+    #[structopt(
+        short,
+        long,
+        default_value = "20",
+        help = "Maximum amount of retries on download failure"
+    )]
     pub tries: usize,
 
     ///Show all logs
-    #[structopt(short, long, help = "Enable more information regarding the scraping process")]
+    #[structopt(
+        short,
+        long,
+        help = "Enable more information regarding the scraping process"
+    )]
     pub verbose: bool,
 
     /// The least seconds of delay between downloads
-    #[structopt(long, default_value = "0", help="Add a delay in seconds between downloads to reduce the likelihood of getting banned")]
+    #[structopt(
+        long,
+        default_value = "0",
+        help = "Add a delay in seconds between downloads to reduce the likelihood of getting banned"
+    )]
     pub delay: u64,
 
     /// The max seconds of delay between downloads
-    #[structopt(long, default_value = "0", help="Generate an extra random delay between downloads, from 0 to this number. This is added to the base delay seconds")]
+    #[structopt(
+        long,
+        default_value = "0",
+        help = "Generate an extra random delay between downloads, from 0 to this number. This is added to the base delay seconds"
+    )]
     pub random_range: u64,
 
     /// User agent to be used to send requests
-    #[structopt(short, long, default_value = "suckit", help="User agent to be used for sending requests")]
+    #[structopt(
+        short,
+        long,
+        default_value = "suckit",
+        help = "User agent to be used for sending requests"
+    )]
     pub user_agent: String,
 }
 
