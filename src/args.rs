13ecbe69c2a100fa551663d4927680d3036a870f
Esteban Blanc <estblcsk@gmail.com>
2020-07-02 15:17:20 +0200

    misc: Run cargo fmt

--- src/args.rs
+++ src/args.rs
@@ -100,11 +100,7 @@ pub struct Args {
     pub exclude: Regex,
 
     /// Decides if we should bail out on download error (like, too many redirects)
-    #[structopt(
-        short,
-        long,
-        help = "Flag to enable or disable exit on error"
-    )]
+    #[structopt(short, long, help = "Flag to enable or disable exit on error")]
     pub continue_on_error: bool,
 }
 
