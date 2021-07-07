0b1b6ac3a70f3034d80f10e67962b4073c63b8e3
pinkforest <36498018+pinkforest@users.noreply.github.com>
2021-07-08 04:11:44 +1000

    Initialises base lib-bin structure

--- src/bin/suckit.rs
+++ src/bin/suckit.rs
@@ -1,16 +1,8 @@
-mod args;
-mod disk;
-mod dom;
-mod downloader;
-mod logger;
-mod response;
-mod scraper;
-mod url_helper;
-
-use scraper::Scraper;
+use suckit::args::Args;
+use suckit::scraper::Scraper;
 
 fn main() {
-    let args = args::Args::collect();
+    let args = Args::collect();
 
     let mut scraper = Scraper::new(args);
 
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+pub mod args;
+pub mod disk;
+pub mod dom;
+pub mod downloader;
+pub mod logger;
+pub mod response;
+pub mod scraper;
+pub mod url_helper;
