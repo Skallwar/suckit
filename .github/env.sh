addd0e75c1633374744817cdd44145683b3843fb
Esteban Blanc <esteban.blanc@epita.fr>
2022-05-11 15:16:32 +0200

    misc: Revert to openssl
    We switched some time ago to rustls in order to cross compile more
    easilly but it depends on `ring` which in full assembly and does not the
    future: RISC-V
    
    This revert to openssl while making sure the CI still cross-compile
    correctly to aarch64

---
+++ .github/env.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+if [ $# != 1 ]; then
+	echo "Usage: . $0 <rust-target>"
+	exit 1
+fi
+
+arch=""
+sysroot=""
+need_target_linker=""
+ubuntu_cross_pkg_list=""
+target_linker=""
+target_cc=""
+
+case "$1" in
+	aarch64*)
+		arch="arm64"
+		sysroot="/usr/lib/aarch64-linux-gnu/"
+		ubuntu_cross_pkg_list="gcc-aarch64-linux-gnu"
+		# YES they are the same but otherwise it fails
+		target_linker="aarch64-linux-gnu-gcc"
+		target_cc="aarch64-linux-gnu-gcc"
+
+	;;
+	x86_64*)
+		target_linker="gcc"
+		target_cc="gcc"
+	;;
+esac
+
+echo MULTILIB_ARCH=$arch
+echo PKG_CONFIG_SYSROOT_DIR=$sysroot
+echo TARGET_CC=$target_cc
+echo CARGO_TARGET_$(echo "$1" | tr 'a-z' 'A-Z' | tr '-' '_' )_LINKER=$target_linker
+echo UBUNTU_CROSS_PKG_LIST=$ubuntu_cross_pkg_list
