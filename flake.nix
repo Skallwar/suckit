ff492b83ebb7620cf6eb79b26770cf05f51f5b12
Esteban Blanc <eblanc@baylibre.com>
2023-01-14 13:50:06 +0100

    build: add flake.nix for nix users

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "SuckIT devshell";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-22.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShell = mkShell {
+          buildInputs = [
+            rust-bin.stable.latest.default
+            openssl
+            pkg-config
+          ];
+        };
+      }
+    );
+}
