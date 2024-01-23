e47b71fe36f55be9b1549a80ab5758e348476333
Esteban Blanc <estblcsk@gmail.com>
2024-01-23 19:12:08 +0100

    misc: Update Nix flake
    Signed-off-by: Esteban Blanc <estblcsk@gmail.com>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "SuckIT devshell";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-23.11";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url  = "github:numtide/flake-utils";
   };
